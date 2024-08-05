583ccbfc47f6ce4bc7c0d41fefb25a4910371961
Kirill Bastrikov <380929@gmail.com>
2024-08-05 21:42:02 +0800

    Исправлен экшн гитхаба

--- migration_container/migration.sh
+++ migration_container/migration.sh
@@ -3,4 +3,4 @@ source .env
 
 export MIGRATION_DSN="host=postgresql port=$POSTGRES_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=disable"
 
-sleep 2 && goose -dir "${MIGRATION_PATH}" postgres "${MIGRATION_DSN}" up -v
+sleep 2 && /bin/goose -dir "${MIGRATION_PATH}" postgres "${MIGRATION_DSN}" up -v
