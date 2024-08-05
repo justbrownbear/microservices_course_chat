4f3c2e277c4e66c6ea2e8a2a5e763e6734838c73
Kirill Bastrikov <380929@gmail.com>
2024-08-05 21:34:29 +0800

    Исправлен экшн гитхаба

--- migration_container/migration.sh
+++ migration_container/migration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source .env.local
+source .env
 
-export MIGRATION_DSN="host=postgresql port=5432 dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=disable"
+export MIGRATION_DSN="host=postgresql port=$POSTGRES_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=disable"
 
 sleep 2 && goose -dir "${MIGRATION_PATH}" postgres "${MIGRATION_DSN}" up -v
