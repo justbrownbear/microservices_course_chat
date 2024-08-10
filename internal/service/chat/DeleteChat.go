3abeb210774dbcf039f0b57c7790f413ed4f1541
Kirill Bastrikov <380929@gmail.com>
2024-08-10 11:56:13 +0800

    Исправлены замечания по ревью

--- internal/service/chat/DeleteChat.go
+++ internal/service/chat/DeleteChat.go
@@ -4,6 +4,9 @@ import "context"
 
 func (chatServiceInstance *chatService) DeleteChat(ctx context.Context, chatID uint64) error {
 	err := chatServiceInstance.repository.DeleteChat(ctx, int64(chatID))
+	if(err != nil) {
+		return err
+	}
 
 	return err
 }
