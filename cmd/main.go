c4a3219bc243066e9db4c379c185ab43c9b0c45a
Kirill Bastrikov <380929@gmail.com>
2024-08-10 13:00:18 +0800

    Реализовано корректное высвобождение ресурсов приложения при получении сигнала SIGTERM

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/fatih/color"
 
@@ -31,10 +33,28 @@ func main() {
 	gRPCProtocol := grpcConfig.GetGrpcProtocol()
 	gRPCHost := grpcConfig.GetGrpcHost()
 	gRPCPort := grpcConfig.GetGrpcPort()
-	err := app.StartApp(gRPCProtocol, gRPCHost, gRPCPort)
-	if err != nil {
-		log.Fatalf(color.RedString("Failed to start app: %v", err))
-	}
+
+	// Создаем канал в котором будем ловить сигналы ОС
+	stopChannel := make(chan os.Signal, 1)
+
+	// Регистрируем сигналы, которые будем ловить в канале stopChannel
+	signal.Notify(stopChannel, os.Interrupt, syscall.SIGTERM)
+
+	// Чтобы не блокировать основной поток, запускаем сервер в горутине
+	go func() {
+		err := app.StartApp(gRPCProtocol, gRPCHost, gRPCPort)
+		if err != nil {
+			log.Println(color.RedString("Failed to start app: %v", err))
+			// Отправляем SIGTERM в канал, чтобы корректно завершить приложение и закрыть ресурсы
+			stopChannel <- syscall.SIGTERM
+		}
+	}()
+
+	// Как я понимаю, тут мы застрянем до тех пор, пока не придет сигнал
+	<-stopChannel
+
+	// А дальше отработает defer
+	log.Println( "Shutting down app..." )
 }
 
 func getConfig() (config.GRPCConfig, config.PostgresqlConfig) {
